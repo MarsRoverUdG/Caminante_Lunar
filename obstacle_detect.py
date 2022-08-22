d1a76fa886c2752fe770b5869a740824d9d83462
Jorge Madrid <jolumago79@gmail.com>
2022-08-22 16:38:36 -0500

    The algorithm to detect the objects was fixed

--- obstacle_detect.py
+++ obstacle_detect.py
@@ -5,19 +5,32 @@ import rospy
 import numpy as np
 from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import LaserScan
+import math
 
 
 def callback_laser(msg):
-    global distances
-    global angles
-    laser = np.asarray(msg.ranges[90:631])
+    global distances    # Obstacle distances
+    global angles       # Obstacle angles (respect the robot ref frame)
+    # There are 721 laser data, from -120 to 120 degrees
+    # We need only from -90 to 90 degrees, so we don care about the first and the last 90 data
+    laser_sec = np.zeros((30,2))    # Section array (distance and index) 30 elements per section
+    min_sec = np.zeros((18,2))      # Section minimum (distance and index) 18 sections
 
-    laser_sec = np.zeros((18, 30))
-    min_sec = np.zeros((18,3))
-    for i in range(18):
-        laser_sec[i, :] = laser[30*i:30*(i)+30]
-        min_sec[i,1] = np.amin(laser_sec[i, :])
-        min_sec[i,2] = (90+np.argmin(laser_sec[i, :]) + 30*i) * msg.angle_increment
+    ### Object Dectection algorithm ###
+    for i in range(0,18):   # Number of dectections (18)
+        laser_sec[:,0] = np.asarray([msg.ranges[i*30 + 90:i*30 + 120]])         # Get the distances of the section data
+        laser_sec[:,1] = np.asarray([j for j in range(i*30 + 90,i*30 + 120)])   # Get the index of the section data
+        min_sec[i,0] = np.amin(laser_sec[:,0])                                  # Get the minimum distance of the section data
+        min_sec[i,1] = laser_sec[np.argmin(laser_sec[:,0]),1]                   # Get the index of the minimum
+    min_sec[:,1] = min_sec[:,1]*msg.angle_increment -120*math.pi/180            # Transform the idex to angle (rad) respect the robot ref frame
+
+    #laser = np.asarray(msg.ranges[90:631])
+    #laser_sec = np.zeros((18, 30))
+    #min_sec = np.zeros((18,3))
+    #for i in range(18):
+    #    laser_sec[i, :] = laser[30*i:30*(i)+30]
+    #    min_sec[i,1] = np.amin(laser_sec[i, :])
+    #    min_sec[i,2] = (90+np.argmin(laser_sec[i, :]) + 30*i) * msg.angle_increment
     distances = min_sec[:,1].tolist()
     angles = min_sec[:,2].tolist()
 
