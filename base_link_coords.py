c0cd15de60e169d5c204290ce8634b847bcb9f14
Jorge Madrid <jolumago79@gmail.com>
2022-08-26 13:46:36 -0500

    The algorithm works (mostly)

--- base_link_coords.py
+++ base_link_coords.py
@@ -7,27 +7,15 @@
 import rospy
 import tf
 import tf2_ros
-import numpy as np
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Int32
 from geometry_msgs.msg import PoseStamped
     
-def PoseStamped_to_pose(msg):
-    global meta_coords
-    # PoseStamped_to_pose2d() convierte un mensaje tipo PoseStamped a una pose 2d en un arreglo
-    # de Numpy con la forma (x,y,theta) con theta en radianes.
-    
-    x = msg.pose.position.x
-    y = msg.pose.position.y
-    (_, _, theta) = tf.transformations.euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
-
-    meta_coords =  list([x, y, theta])
-
 def get_base_link_pose():
-    # get_base_link_pose() regresa un arreglo en Numpy con la pose de la base del robot con
-    # respecto al marco de referencia global en la forma (x, y, theta) con theta en radianes.
-    
+    # get_base_link_pose() regresa un lista con la pose de la base del base_link con
+    # respecto al marco de referencia global en la forma [x, y, theta] con theta en radianes.
+    global time
     trans = get_base_link_coords()
-    
+    time = trans.header.stamp.to_sec()
     x = trans.transform.translation.x
     y = trans.transform.translation.y
     (_, _, theta) = tf.transformations.euler_from_quaternion([trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w])
@@ -43,12 +31,25 @@ def get_base_link_coords():
             print ('waiting for tf')
             trans = 0
 
+def PoseStamped_to_pose(msg):
+    # PoseStamped_to_pose2d() convierte un mensaje tipo PoseStamped a una pose 2d en un arreglo
+    # de Numpy con la forma (x,y,theta) con theta en radianes.
+    global meta_coords
+    x = msg.pose.position.x
+    y = msg.pose.position.y
+    (_, _, theta) = tf.transformations.euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+
+    meta_coords =  list([x, y, theta])
+
 if __name__ == '__main__':
     global meta_coords
+    global time
+    time = 0
     rospy.init_node("base_link_coords")
-    rospy.Subscriber("/meta_competencia", PoseStamped,PoseStamped_to_pose)
+    rospy.Subscriber("/meta_competencia", PoseStamped, PoseStamped_to_pose)
     pub_coords = rospy.Publisher("/base_link_coords", Float32MultiArray, queue_size=10)
     pub_meta_coords = rospy.Publisher("/meta_coords", Float32MultiArray, queue_size=10)
+    pub_time = rospy.Publisher("/time",Int32,queue_size=10)
     # Iniciar el árbol de transformaciones. Se necesita un delay para dar tiempo a cargar los
     # marcos de referencia.
     tfBuffer = tf2_ros.Buffer()
@@ -56,19 +57,21 @@ if __name__ == '__main__':
     rospy.sleep(1)
     
     loop = rospy.Rate(10)
-    
+    meta_coords = False
     while not rospy.is_shutdown():
         if meta_coords != False:
             base_coords = get_base_link_pose()
+            msg_time = Int32()
             msg_base_coords = Float32MultiArray()
             msg_meta_coords = Float32MultiArray()
+            msg_time = int(time)
             msg_base_coords.data = base_coords
             msg_meta_coords.data = meta_coords
             pub_coords.publish(msg_base_coords)
             pub_meta_coords.publish(msg_meta_coords)
-            print("Meta: ",meta_coords)
-            print("Base: ",base_coords)
-        
+            pub_time.publish(msg_time)
+            print("Pose Meta: ",meta_coords)
+            print("Pose Base: ",base_coords)
         loop.sleep()
 
     
--- control_algorithm.py
+++ control_algorithm.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-from random import randint, random, randrange
 import rospy
 import numpy as np
 import math
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Int32
 from geometry_msgs.msg import Twist
-import matplotlib.pyplot as plt
+
+#--------Callback Functions-------------
+def callback_time(msg):
+    global reloj
+    reloj = msg.data
 
 def callback_obstacle_angles(msg):
     global obstacle_angles
@@ -23,42 +26,49 @@ def callback_meta_pos(msg):
     global meta_pos
     meta_pos = np.asarray(msg.data)
 
-
+#----------------Main--------------------
 if __name__ == '__main__':
     global obstacle_angles
     global obstacle_distances
     global base_pos
     global meta_pos
+    global reloj
+    ## Control Parameters
     k_att = 1
-    k_rep = 1.5
-    kvx = 2
-    kvy = 2
-    kw = 4
-    h = 0.1
-    d_max = 1.0
+    k_rep = 3
+    kvx = 1
+    kvy = 1
+    kw = 0.6
+    h = 0.15
+    d_max = 0.8
+    # Ros Node Config
     rospy.init_node("control_algorithm_node")
+    rospy.Subscriber("/time",Int32,callback_time)
     rospy.Subscriber("/obstacle_detect_angles",Float32MultiArray,callback_obstacle_angles)
     rospy.Subscriber("/obstacle_detect_distances",Float32MultiArray,callback_obstacle_distances)
     rospy.Subscriber("/base_link_coords", Float32MultiArray,callback_base_pos)
     rospy.Subscriber("/meta_coords",Float32MultiArray,callback_meta_pos)
     pub_cmd_vel = rospy.Publisher("/hsrb/command_velocity", Twist, queue_size=10)
     loop = rospy.Rate(10)
-    obstacle_angles = np.zeros((18))
-    obstacle_distances = np.zeros((18))
-    base_pos = np.zeros((3))
+    # Initial state
+    n_sections = 24
+    obstacle_angles = np.zeros((n_sections))
+    obstacle_distances = np.zeros((n_sections))
+    base_pos = np.asarray([2,2,2])
     meta_pos = np.zeros((3))
-    X = []
-    Y = []
-    bx = []
-    by = []
+    reloj = 0
+    inicio = 0
+    final = 0
     while not rospy.is_shutdown():
+        if reloj != 0 and inicio == 0:
+            inicio = reloj
+            print("Inicio: ",inicio)
         ## CONTROL ALGORITHM ##
-        
         #Force vectors calculus
         F_att = k_att*(meta_pos[0:2] - base_pos[0:2])
         F_rep_x = k_rep*(obstacle_distances - d_max)*np.cos(obstacle_angles + base_pos[2])
         F_rep_y = k_rep*(obstacle_distances - d_max)*np.sin(obstacle_angles + base_pos[2])
-        print("e_d: ",obstacle_distances - d_max)
+
         F_x = F_att[0] + np.sum(F_rep_x)
         F_y = F_att[1] + np.sum(F_rep_y)
         #Force normalization
@@ -68,32 +78,24 @@ if __name__ == '__main__':
         # Gradient step
         x_d = base_pos[0] + h*normF_x 
         y_d = base_pos[1] + h*normF_y 
-        
-        X.append(x_d)
-        Y.append(y_d)
-        bx.append(base_pos[0])
-        by.append(base_pos[1])
-        if len(X)==200:
-            plt.figure()
-            plt.plot(X,Y,'ro',bx,by,'b-')
-            plt.grid(True)
-            plt.show()
-        
         #theta_d = math.atan2(y_d - base_pos[1],x_d - base_pos[0])
         theta_d = math.atan2(meta_pos[1] - base_pos[1],meta_pos[0] - base_pos[0])
-        #theta_d = meta_pos[2]
-        if math.sqrt(math.pow(meta_pos[0] - x_d,2) + math.pow(meta_pos[1] - y_d,2)) <=0.5:
+        # If the step is close enough to the goal, then the reference is the goal
+        if math.sqrt(math.pow(meta_pos[0] - x_d,2) + math.pow(meta_pos[1] - y_d,2)) <=0.6:
             x_d = meta_pos[0]
             y_d = meta_pos[1]
             theta_d = meta_pos[2]
-        # Errors
-        print("xd ",x_d,", ",y_d,", ",theta_d)
+        print("Reference: ",x_d,", ",y_d,", ",theta_d)
+        # Errors calculations
         error_x = x_d - base_pos[0]
         error_y = y_d - base_pos[1]
         error_w = math.atan2(math.sin(theta_d-base_pos[2]),math.cos(theta_d-base_pos[2]))
         # Message sending
         msg_cmd_vel = Twist()
-        if math.sqrt(math.pow(meta_pos[0] - base_pos[0],2) + math.pow(meta_pos[1] - base_pos[1],2)) >= 0.1:
+        # Distance between the robot base and the goal
+        dis_error = math.sqrt(math.pow(meta_pos[0] - base_pos[0],2) + math.pow(meta_pos[1] - base_pos[1],2))
+        # Condition to continue or stop the control proccess
+        if dis_error >= 0.01 or error_w >= 0.01:
             msg_cmd_vel.linear.x = kvx*(error_x*math.cos(base_pos[2]) + error_y*math.sin(base_pos[2]))
             msg_cmd_vel.linear.y = kvy*(-error_x*math.sin(base_pos[2]) + error_y*math.cos(base_pos[2]))
             msg_cmd_vel.angular.z = kw*error_w
@@ -101,6 +103,11 @@ if __name__ == '__main__':
             msg_cmd_vel.linear.x = 0
             msg_cmd_vel.linear.y = 0
             msg_cmd_vel.angular.z = 0
+            final = reloj
+            print("Inicio: ",inicio," segundos")
+            print("Final : ",final," segundos")
+            print("Duración: ",final - inicio," segundos")
+            break
         pub_cmd_vel.publish(msg_cmd_vel)        
         
         loop.sleep()
