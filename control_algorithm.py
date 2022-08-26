0eaa2a2dbad12da30ca14f2566ed658cb6477a68
Jorge Madrid <jolumago79@gmail.com>
2022-08-25 21:45:54 -0500

    Ya es totalmente omnidireccional

--- control_algorithm.py
+++ control_algorithm.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+from random import randint, random, randrange
 import rospy
 import numpy as np
 import math
 from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import Twist
+import matplotlib.pyplot as plt
 
 def callback_obstacle_angles(msg):
     global obstacle_angles
@@ -27,73 +29,78 @@ if __name__ == '__main__':
     global obstacle_distances
     global base_pos
     global meta_pos
-    
     k_att = 1
-    k_rep = 1
-    kvx = 0.6
-    kvy = 0.6
-    kw = 0.3
-    h = 0.5
-    d_max = 1.5
-    
+    k_rep = 1.5
+    kvx = 2
+    kvy = 2
+    kw = 4
+    h = 0.1
+    d_max = 1.0
     rospy.init_node("control_algorithm_node")
     rospy.Subscriber("/obstacle_detect_angles",Float32MultiArray,callback_obstacle_angles)
     rospy.Subscriber("/obstacle_detect_distances",Float32MultiArray,callback_obstacle_distances)
     rospy.Subscriber("/base_link_coords", Float32MultiArray,callback_base_pos)
-    rospy.Subscriber("/meta_competencia_x_y_theta",Float32MultiArray,callback_meta_pos)
+    rospy.Subscriber("/meta_coords",Float32MultiArray,callback_meta_pos)
     pub_cmd_vel = rospy.Publisher("/hsrb/command_velocity", Twist, queue_size=10)
     loop = rospy.Rate(10)
     obstacle_angles = np.zeros((18))
     obstacle_distances = np.zeros((18))
     base_pos = np.zeros((3))
     meta_pos = np.zeros((3))
+    X = []
+    Y = []
+    bx = []
+    by = []
     while not rospy.is_shutdown():
         ## CONTROL ALGORITHM ##
         
         #Force vectors calculus
         F_att = k_att*(meta_pos[0:2] - base_pos[0:2])
-        F_rep_x = -k_rep*(pow(obstacle_distances - d_max, 4))*np.cos(obstacle_angles + base_pos[2])*[obstacle_distances <= d_max]
-        F_rep_y = -k_rep*(pow(obstacle_distances - d_max, 4))*np.sin(obstacle_angles + base_pos[2])*[obstacle_distances <= d_max]
+        F_rep_x = k_rep*(obstacle_distances - d_max)*np.cos(obstacle_angles + base_pos[2])
+        F_rep_y = k_rep*(obstacle_distances - d_max)*np.sin(obstacle_angles + base_pos[2])
+        print("e_d: ",obstacle_distances - d_max)
         F_x = F_att[0] + np.sum(F_rep_x)
         F_y = F_att[1] + np.sum(F_rep_y)
         #Force normalization
-        distance_left = math.sqrt(math.pow(meta_pos[0]-base_pos[0],2) + math.pow(meta_pos[0]-base_pos[0],2))
-        normF_x = F_x if distance_left <=0.3 else F_x/math.sqrt(math.pow(F_x,2) + math.pow(F_y,2))
-        normF_y = F_y if distance_left <=0.3 else F_y/math.sqrt(math.pow(F_x,2) + math.pow(F_y,2))
+        distance_left = math.sqrt(math.pow(meta_pos[0]-base_pos[0],2) + pow(meta_pos[1]-base_pos[1],2))
+        normF_x = F_x if distance_left <=1.0 else F_x/math.sqrt(math.pow(F_x,2) + math.pow(F_y,2))
+        normF_y = F_y if distance_left <=1.0 else F_y/math.sqrt(math.pow(F_x,2) + math.pow(F_y,2))
         # Gradient step
-        x_d = base_pos[0] + h*normF_x
-        y_d = base_pos[1] + h*normF_y
-        theta_d = math.atan2(y_d - base_pos[1],x_d - base_pos[0])
+        x_d = base_pos[0] + h*normF_x 
+        y_d = base_pos[1] + h*normF_y 
+        
+        X.append(x_d)
+        Y.append(y_d)
+        bx.append(base_pos[0])
+        by.append(base_pos[1])
+        if len(X)==200:
+            plt.figure()
+            plt.plot(X,Y,'ro',bx,by,'b-')
+            plt.grid(True)
+            plt.show()
+        
+        #theta_d = math.atan2(y_d - base_pos[1],x_d - base_pos[0])
+        theta_d = math.atan2(meta_pos[1] - base_pos[1],meta_pos[0] - base_pos[0])
         #theta_d = meta_pos[2]
-        if math.sqrt(math.pow(meta_pos[0] - x_d,2) + math.pow(meta_pos[1] - y_d,2)) <=0.1:
+        if math.sqrt(math.pow(meta_pos[0] - x_d,2) + math.pow(meta_pos[1] - y_d,2)) <=0.5:
             x_d = meta_pos[0]
             y_d = meta_pos[1]
             theta_d = meta_pos[2]
+        # Errors
+        print("xd ",x_d,", ",y_d,", ",theta_d)
+        error_x = x_d - base_pos[0]
+        error_y = y_d - base_pos[1]
         error_w = math.atan2(math.sin(theta_d-base_pos[2]),math.cos(theta_d-base_pos[2]))
         # Message sending
         msg_cmd_vel = Twist()
-        u_x = x_d - base_pos[0]
-        u_y = y_d - base_pos[1]
-        if math.sqrt(math.pow(meta_pos[0] - base_pos[0],2) + math.pow(meta_pos[1] - base_pos[1],2)) >= 0.01:
-
-            msg_cmd_vel.linear.x = kvx*math.sqrt(u_x**2 + u_y**2)
-            msg_cmd_vel.linear.y = 0
-            msg_cmd_vel.angular.z = math.atan2(u_y,u_x)-base_pos[2]
-            print('u_x ', u_x, 'u_y', u_y)
+        if math.sqrt(math.pow(meta_pos[0] - base_pos[0],2) + math.pow(meta_pos[1] - base_pos[1],2)) >= 0.1:
+            msg_cmd_vel.linear.x = kvx*(error_x*math.cos(base_pos[2]) + error_y*math.sin(base_pos[2]))
+            msg_cmd_vel.linear.y = kvy*(-error_x*math.sin(base_pos[2]) + error_y*math.cos(base_pos[2]))
+            msg_cmd_vel.angular.z = kw*error_w
         else:
             msg_cmd_vel.linear.x = 0
             msg_cmd_vel.linear.y = 0
-            msg_cmd_vel.angular.z = math.atan2(math.sin(u_y),math.cos(u_x))
-            
-
-        #print("error x: ",x_d - base_pos[0])
-        #print("error y: ",y_d - base_pos[1])
-        #print('F_attX: ', F_att[0], 'F_attY:', F_att[1])
-        #print('F_repX: ', np.sum(F_rep_x), 'F_repY:', np.sum(F_rep_y))
-        #print('x_d ', h*normF_x, 'y_d', h*normF_y)
-        #print(msg_cmd_vel)
+            msg_cmd_vel.angular.z = 0
         pub_cmd_vel.publish(msg_cmd_vel)        
         
         loop.sleep()
-
-    
--- obstacle_detect.py
+++ obstacle_detect.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+# Detects the obstacles and select only 18 obstacle points
+
 from pickletools import uint8
 import rospy
 import numpy as np
 from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import LaserScan
 import math
-
 
 def callback_laser(msg):
     global distances    # Obstacle distances
@@ -15,15 +16,19 @@ def callback_laser(msg):
     # We need only from -90 to 90 degrees, so we don care about the first and the last 90 data
     laser_sec = np.zeros((30,2))    # Section array (distance and index) 30 elements per section
     min_sec = np.zeros((18,2))      # Section minimum (distance and index) 18 sections
-
+    d_max = 1.0
     ### Object Dectection algorithm ###
     for i in range(0,18):   # Number of dectections (18)
         laser_sec[:,0] = np.asarray([msg.ranges[i*30 + 90:i*30 + 120]])         # Get the distances of the section data
         laser_sec[:,1] = np.asarray([j for j in range(i*30 + 90,i*30 + 120)])   # Get the index of the section data
-        min_sec[i,0] = np.amin(laser_sec[:,0])  if np.amin(laser_sec[:,0]) <= 1.5 else 0.0                       # Get the minimum distance of the section data
-        min_sec[i,1] = laser_sec[np.argmin(laser_sec[:,0]),1]                   # Get the index of the minimum
+        #min_sec[i,0] = np.amin(laser_sec[:,0]) if np.amin(laser_sec[:,0]) <= 1.5 else 0.0                       # Get the minimum distance of the section data
+        min_sec[i,0] = laser_sec[0,0] if laser_sec[0,0] <= d_max else d_max
+        #min_sec[i,1] = laser_sec[np.argmin(laser_sec[:,0]),1]                   # Get the index of the minimum
+        min_sec[i,1] = laser_sec[0,1]
     min_sec[:,1] = min_sec[:,1]*msg.angle_increment -120*math.pi/180            # Transform the idex to angle (rad) respect the robot ref frame
 
+    #print(min_sec)
+    #print("==========================")
     #laser = np.asarray(msg.ranges[90:631])
     #laser_sec = np.zeros((18, 30))
     #min_sec = np.zeros((18,3))
@@ -31,10 +36,13 @@ def callback_laser(msg):
     #    laser_sec[i, :] = laser[30*i:30*(i)+30]
     #    min_sec[i,1] = np.amin(laser_sec[i, :])
     #    min_sec[i,2] = (90+np.argmin(laser_sec[i, :]) + 30*i) * msg.angle_increment
+    #print(min_sec)
+    #print("==========================")
     distances = min_sec[:,0].tolist()
     angles = min_sec[:,1].tolist()
 
 def main():
+    # Node config
     global distances
     global angles
     rospy.init_node("obstacle_detect_node")
@@ -55,7 +63,9 @@ def main():
             msg_distances = Float32MultiArray()
             msg_distances.data = distances
             pub_distances.publish(msg_distances)
-            print("Angles and distances are publishing")
+            #print("Angles and distances are publishing")
+            print("distances: ",distances)
+            print("angles: ",angles)
         loop.sleep()
 if __name__ == '__main__':
     try:
